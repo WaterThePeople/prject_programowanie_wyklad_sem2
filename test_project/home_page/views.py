8cb408a1ced4897ce0533666b15ca9165026243e
Water The People <magnificentmax0@gmail.com>
2022-05-16 22:08:18 +0200

    xd

--- test_project/home_page/views.py
+++ test_project/home_page/views.py
@@ -1,26 +1,67 @@
 from django.shortcuts import render
-from .models import Post
-
-posts = [
-    {
-        'author':'MAX',
-        'title':'Blog Post 1',
-        'content':'First post',
-        'date_posted':'11 May 2020'
-    },
-    {
-        'author':'MEKS',
-        'title':'Blog Post 2',
-        'content':'Second post',
-        'date_posted':'26 June 2021'
-    }
-]
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView
+from django.views.generic import DetailView,CreateView,UpdateView,DeleteView
+from .models import Post,Comment
+from .forms import CommentForm
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request,'home_page/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home_page/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class AddCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'home_page/add_comment.html'
+
+    def form_valid(self,form):
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    success_url = '/'
+
 
 def welcome(request):
     return render(request,'home_page/welcome.html',{'title':'Welcome'})
